c644be6a116a1c2c1e0b4d9e09872e96433c5161
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-06-18 17:54:18 +0800

    test moralis

--- controller.js
+++ controller.js
@@ -1,8 +1,40 @@
-import { AJAX } from "./helper.js";
-import { ETHER_API_KEY } from "./helper.js";
+/* Moralis init code */
+const serverUrl = "https://w4jt9wczr0ur.usemoralis.com:2053/server";
+const appId = "cOhVzIVvs2EJ3t87SQD28iBHG93KyNm8ReVhaToF";
+Moralis.start({ serverUrl, appId });
 
-const wallet = "0x1477AaccD85079AFF8c8e0d54A65c245CCEcBaDd";
+/* TODO: Add Moralis Authentication code */
 
-const singleAccountBalance = `https://api.etherscan.io/api?module=account&action=balance&address=${wallet}&tag=latest&apikey=${ETHER_API_KEY}`;
+/* Authentication code */
+async function login() {
+  try {
+    // let user = Moralis.User.current();
+    // if (!user) {
+    //   user = await Moralis.authenticate({
+    //     signingMessage: "Log in using Moralis",
+    //   })
+    //     .then(function (user) {
+    //       console.log("logged in user:", user);
+    //       console.log(user.get("ethAddress"));
+    //       // console.log(user.getAllERC20());
+    //     })
+    //     .catch(function (error) {
+    //       console.log(error);
+    //     });
+    // }
+    const balances = await Moralis.Web3.getAllERC20({
+      address: "0x7c04c40Eeb8Ab6B645c2739F69c72AA7f10E4AdB",
+    });
+    console.log(balances);
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-AJAX(singleAccountBalance);
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
